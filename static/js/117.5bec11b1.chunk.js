26f8915a42541625ab36a3293e78bb00c21c1fb0
Obie Kaku <obiekaku@gmail.com>
2025-01-11 11:23:16 -0600

    Updates

---
+++ static/js/117.5bec11b1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkarchitects_spa=self.webpackChunkarchitects_spa||[]).push([[117],{19117:(t,r,e)=>{e.d(r,{NameService:()=>c});var s=e(64467);class c{constructor(t,r){(0,s.A)(this,"client",void 0),(0,s.A)(this,"registry",void 0),this.client=t,this.registry=r}async resolveName(t){try{const{contract:r,getQueryMsg:e}=this.registry;return await this.client.queryContractSmart(r,e(t))}catch(e){var r;return void(null===(r=console)||void 0===r||r.error(e))}}}}}]);
+//# sourceMappingURL=117.5bec11b1.chunk.js.map
